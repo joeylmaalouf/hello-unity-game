204c6f41e85b4e61b3d6887178d533ccfb172b3a
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-11-24 22:57:07 -0500

    added player controls

---
+++ hello-unity-game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+  public float speed;
+
+  void FixedUpdate()
+  {
+    Vector3 movement = new Vector3
+    (
+      Input.GetAxis("Horizontal"),
+      0.0f,
+      Input.GetAxis("Vertical")
+    );
+    rigidbody.AddForce(movement * speed * Time.deltaTime);
+  }
+}
