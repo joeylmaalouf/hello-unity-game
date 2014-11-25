281279dbbf6f1880b6733b69ef6dd050d5e5492d
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-11-24 23:09:26 -0500

    made camera move with player

---
+++ hello-unity-game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject player;
+	private Vector3 offset;
+
+	void Start()
+	{
+		this.offset = this.transform.position;
+	}
+
+	void LateUpdate()
+	{
+		this.transform.position = this.player.transform.position + this.offset;
+	}
+}
--- hello-unity-game/Assets/Scripts/PlayerController.cs
+++ hello-unity-game/Assets/Scripts/PlayerController.cs
@@ -3,16 +3,17 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-  public float speed;
+	public float speed;
 
-  void FixedUpdate()
-  {
-    Vector3 movement = new Vector3
-    (
-      Input.GetAxis("Horizontal"),
-      0.0f,
-      Input.GetAxis("Vertical")
-    );
-    rigidbody.AddForce(movement * speed * Time.deltaTime);
-  }
+	void FixedUpdate()
+	{
+		Vector3 movement = new Vector3
+		(
+			Input.GetAxis("Horizontal"),
+			0.0f,
+			Input.GetAxis("Vertical")
+		);
+
+		this.rigidbody.AddForce(movement * this.speed * Time.deltaTime);
+	}
 }
